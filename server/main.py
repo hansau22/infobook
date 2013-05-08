63f7929ae19887514ff2e8fb5bb2f999eafeb596
hansau22 <hansau22@tormail.org>
2013-05-08 15:30:39 +0200

    encryption now working

--- server/main.py
+++ server/main.py
@@ -1,5 +1,7 @@
 import socket
 import re
+from Crypto.Cipher import Blowfish
+from Crypto import Random
 import random
 
 class connectionHandler:
@@ -9,7 +11,7 @@ class connectionHandler:
         
         
         serv_soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        serv_soc.bind(("", 32322))
+        serv_soc.bind(("", 32323))
         serv_soc.listen(1)
         
         try:
@@ -21,13 +23,17 @@ class connectionHandler:
                     if not data: 
                         komm.close() 
                         break
+                        
+                    if self.is_encrypted(data):
+                        data = self.decrypt(data)
 
+                    print "data:  "+data
                     data = re.split(";", data)
                     header = self.parse_header(data[0])
                     data = data[1]
 
                     if header[0]== "dhex":
-                        resp = self.init_dh(data)
+                        resp = self.init_dh(data)                      
                     elif header[0].lowercase() == "auth":
                         resp = self.auth_user(data)
                     elif header[0].lowercase() == "mesg":
@@ -46,19 +52,46 @@ class connectionHandler:
     def parse_header(self, data):
         return re.split(":", data)
         
+    def is_encrypted(self, data):
+        tmp = re.split(":", data)
+        if tmp[0] == "dhex":
+            return False
+        return True
+        
     # generates the AES sessionkey based on the algorithm of Diffie-Hellman
 
     def init_dh(self, data):
+        print "a:  " + data
         sessid = self.sid_pool
         self.sid_pool += 1
-        proot = 2
-        prime = 2959758 # related 6144-group RFC 3546
+        proot = 3
+        prime = 2959259
+        #prime = 13
         num = random.randrange(1, prime - 2, 1)
         b = proot**num % prime
+        print "b:  " + str(b)
         resp = str(sessid) + ":" + str(b)
-        sesskey = int(data)**proot % prime
+        sesskey = int(data)**num % prime
         self.sesskey.append(sesskey)
+        print "sesskey:  " + str(sesskey)
         return resp
+        
+    def decrypt(self, data):
+        tmp = re.split(";", data)
+        skeyid = re.split(":", tmp[0])
+        skeyid = skeyid[3]
+        tmp = re.split(":", tmp[1])
+        iv = tmp[0]
+        cipher = Blowfish.new(str(self.sesskey[int(skeyid)]), Blowfish.MODE_CFB, "asdfasdf")
+        return cipher.decrypt(tmp[1])
+        
+    def encrypt(self, data):
+        tmp = re.split(":", data)
+        iv = data[1]
+        cipher = Blowfish.new(self.sesskey[int(data[0])], Blowfish.MODE_CFB, int(iv))
+        return data[0] + ":" + iv + ":" + cipher.encrypt(data[2])
+        
+        
         
         
 conn = connectionHandler()
