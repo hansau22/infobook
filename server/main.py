322366764eacd67d11abcca541a70863a59984b6
hansau22 <hansau22@tormail.org>
2013-05-15 08:23:36 +0200

    features improved

--- server/main.py
+++ server/main.py
@@ -11,7 +11,7 @@ class connectionHandler:
         
         
         serv_soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        serv_soc.bind(("", 32323))
+        serv_soc.bind(("", 32322))
         serv_soc.listen(1)
         
         try:
@@ -25,20 +25,21 @@ class connectionHandler:
                         break
                         
                     if self.is_encrypted(data):
-                        data = self.decrypt(data)
+                        body = self.decrypt(data)
 
                     print "data:  "+data
                     data = re.split(";", data)
+                    print data
                     header = self.parse_header(data[0])
                     data = data[1]
 
                     if header[0]== "dhex":
                         resp = self.init_dh(data)                      
-                    elif header[0].lowercase() == "auth":
+                    elif header[0] == "auth":
                         resp = self.auth_user(data)
-                    elif header[0].lowercase() == "mesg":
-                        resp = self.recv_msg(data)
-                    elif header[0].lowercase() == "file":
+                    elif header[0] == "mesg":
+                        resp = self.recv_msg(body)
+                    elif header[0] == "file":
                         resp = self.recv_file(data)
                     
                     komm.send(resp)
@@ -65,8 +66,8 @@ class connectionHandler:
         sessid = self.sid_pool
         self.sid_pool += 1
         proot = 3
-        prime = 2959259
-        #prime = 13
+        #prime = 2959259
+        prime = 13
         num = random.randrange(1, prime - 2, 1)
         b = proot**num % prime
         print "b:  " + str(b)
@@ -90,6 +91,10 @@ class connectionHandler:
         iv = data[1]
         cipher = Blowfish.new(self.sesskey[int(data[0])], Blowfish.MODE_CFB, int(iv))
         return data[0] + ":" + iv + ":" + cipher.encrypt(data[2])
+        
+    def recv_msg(self, data):
+        print data
+        return ""
         
         
         
