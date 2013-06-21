afab460c553ed3e2e856841cdef544fcfb09a4d3
hansau22 <hansau22@tormail.org>
2013-06-21 12:28:25 +0200

    setup.py added

---
+++ server/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='Infolib',
+	version='1.0b1',
+	author='Julian Frielinghaus',
+	url='https://github.com/hansau22/infobook',
+	py_modules=['infolib'],
+      )
+
+
+
