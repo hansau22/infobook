3a5aba64a62f1bbc844a88c40c9491a111697163
hansau22 <hansau22@tormail.org>
2013-06-21 13:27:12 +0200

    new folder structure implemented

--- server/infoserver.py
+++ server/infoserver.py
@@ -1,4 +1,7 @@
-from infolib import *
+from libinfo import ConnectionHandler
+from libinfo import DatabaseHandler
+from libinfo import EncryptionHandler
+from libinfo import Pool
 
        
 conn = ConnectionHandler()
--- server/setup.py
+++ server/setup.py
@@ -1,9 +1,19 @@
 from distutils.core import setup
-setup(name='Infolib',
+setup(name='libinfo',
 	version='1.0b1',
 	author='Julian Frielinghaus',
 	url='https://github.com/hansau22/infobook',
-	py_modules=['infolib'],
+
+#	py_modules = [
+#	'libinfo/ConnectionHandler', 
+#	'libinfo/DatabaseHandler', 
+#	'libinfo/EncryptionHandler', 
+#	'libinfo/Pool'
+#	],
+
+	packages = ["libinfo"],
+	package_dir = {"libinfo" : "libinfo"}, 
+
       )
 
 
